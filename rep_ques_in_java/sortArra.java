ee9f173837e6201d020198d77d49d193e0ad2f64
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-20 14:17:56 +0530

    adding questions

---
+++ rep_ques_in_java/sortArra.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class sortArra {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 9, 30, -20, -2, 0};
+        Arrays.sort(arr); // Sorts the array in ascending order
+        System.out.println(Arrays.toString(arr)); // Prints the sorted array
+    }
+}
