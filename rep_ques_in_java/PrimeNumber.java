d471c1b9f0066ac7aef671259e18a991835c5f1e
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-20 13:08:33 +0530

    adding questions

---
+++ rep_ques_in_java/PrimeNumber.java
@@ -0,0 +1,27 @@
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        int num = 2; // Number to check
+        boolean isPrime = true; // Assume number is prime initially
+
+        // Handle numbers <= 1 (not prime)
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            // Check divisors from 2 to num / 2
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        // Output the result
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
