ae486f9cf881ba73f9be67adbb587daabb0ace42
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-20 12:39:29 +0530

    adding questions

---
+++ rep_ques_in_java/PalindromeString.java
@@ -0,0 +1,19 @@
+public class PalindromeString {
+
+    public static void main(String[] args) {
+        String str = "madam"; // The string to check
+        String reversedStr = ""; // This will store the reversed string
+        
+        // Reverse the string
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversedStr += str.charAt(i);
+        }
+        
+        // Compare the reversed string with the original string
+        if (str.equals(reversedStr)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+}
